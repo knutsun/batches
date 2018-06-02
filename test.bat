401518fe5bbf4aec9e8f2c46423eacdc20531b6e
knutsun <csselph@gmail.com>
2018-06-01 21:15:46 -0600

    update test

--- test.bat
+++ test.bat
@@ -1,27 +1,37 @@
 @echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
-set i=0
-set /a x=0
+set "file=test.txt"
+set /A i=0
 set /a timeCounter=0
-for /f "tokens=1*delims=*" %%a in (test.txt) do (
-	call echo %%i%%
-	call set array[%%i%%]=%%a
-	call set n=%%i%%
-	set build=%%a
-	set build=!build:~6,5!
-	echo !build!
-	set timeVar=%%b
-	rem echo !timeVar!
-	set timeInt=!timeVar::=!
-	set /a timeDifference= timeInt - prevTime
+for /F "usebackq tokens=1*delims=*" %%a in ("%file%") do (
+set timeVar=%%b 
+set currentTime=!timeVar::=!
+set build=%%a
+set build=!build:~6,5!
+echo !build!
+echo.
+echo !currentTime! is at index !i!
+echo previous time is !currentTime!
+if !i!==0,2,4,6 (
+	set /a prevTime=!currentTime!
+	echo.
+) else (
+	set /a timeDifference= currentTime - prevTime
+	set /a timeCounter=timeCounter + timeDifference
 	echo time difference is !timeDifference!
-	set prevTime=!timeVar::=!
-	rem echo %timeCounter%
-	set /a timeCounter=timeCounter + !timeDifference!
-	echo !timeCounter!
-	echo array[%%i%%]
-	set /A i+=1
-
+	echo total time is !timeCounter!
+	echo.
 )
+call set array[%%i%%]=%%b
+call set n=%%i%%
+set /A i+=1
+)
+
+rem prints out each line of text file as an array
+rem for /L %%i in (1,1,%n%) do call echo %%array[%%i]%% 
+rem 	if %%array[%%i]%% == %%array[%%i]%% (
+rem 		echo this is it
+rem 		pause
+rem 	)
 pause
