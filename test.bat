2b8b50b0a9c7ea8f77e94134fb2a16992439d63f
knutsun <csselph@gmail.com>
2018-06-01 23:58:55 -0600

    update test

--- test.bat
+++ test.bat
@@ -4,22 +4,25 @@ setlocal EnableExtensions EnableDelayedExpansion
 set "file=test.txt"
 set /A i=1
 set /a timeCounter=0
+
+
+
 for /F "usebackq tokens=1*delims=*" %%a in ("%file%") do (
-set timeVar=%%b 
 
-set currentHour=%%b
-set currentHour=!currentHour:~0,2!
-set currentMinute=%%b
-set currentMinute=!currentMinute:~3,4!
-set /a currentTime=!currentHour!*60
-set /a currentTime=!currentTime!+!currentMinute!
+	set timeVar=%%b 
+	set currentHour=%%b
+	set currentHour=!currentHour:~0,2!
+	set currentMinute=%%b
+	set currentMinute=!currentMinute:~3,4!
+	set /a currentTime=!currentHour!*60
+	set /a currentTime=!currentTime!+!currentMinute!
 
 
-set build=%%a
-set build=!build:~6,5!
+	set build=%%a
+	set build=!build:~6,5!
 
-set /a divnumber=!i!/2
-set /a sum=!divnumber!*2
+	set /a divnumber=!i!/2
+	set /a sum=!divnumber!*2
 
 if !i! NEQ !sum! (
 	set /a prevHour=!currentHour!
@@ -35,6 +38,6 @@ if !i! EQU !sum! (
 set /A i+=1
 )
 
-echo total time for !build! is !timeCounter! minutes
+echo Total time for !build! is !timeCounter! minutes
 echo.
 pause
