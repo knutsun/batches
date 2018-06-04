a0d64e815cba7018f4d6eddd19617c44c7579a1a
knutsun <csselph@gmail.com>
2018-06-04 15:42:54 -0600

    this time.bat w/ prev tester.bat

--- time.bat
+++ time.bat
@@ -17,19 +17,13 @@ set /p m=Make a Selection:
 echo.
 if %m%==1 (
 set build=4.1.0
-goto recordTime
 )
 if %m%==2 (
 set build=4.1.2
-goto recordTime
 )
 if %m%==3 (
 set build=4.2.0
-goto recordTime
 )
-
-
-:recordTime
 cls
 echo 1: FireTV
 echo 2: AndroidTV
@@ -37,14 +31,37 @@ echo.
 set /p p=Which platform? 
 echo.
 if %p%==1 (
-	set platform=FireTV 
+	set platform=FireTV
+	if %build%==4.1.0 (
+		set file=FireTV_%build%
+	)
+	if %build%==4.1.2 (
+		set file=FireTV_%build%
+	)
+	if %build%==4.2.0 (
+		set file=FireTV_%build%
+	)
 )
 if %p%==2 (
 	set platform=AndroidTV 
+	if %build%==4.1.0 (
+		set file=AndroidTV_%build%
+	)
+	if %build%==4.1.2 (
+		set file=AndroidTV_%build%
+	)
+	if %build%==4.2.0 (
+		set file=AndroidTV_%build%
+	)
 )
 echo Selected build is %build% on platform %platform%
 echo.
 cls
+goto recordTime
+
+rem if platform and build equ *** then file equ platformBuild.txt
+
+:recordTime
 echo 1: start
 echo 2: end 
 echo.
@@ -53,14 +70,14 @@ if %selection%==1 goto recordStartTime
 if %selection%==2 goto recordEndTime
 
 :recordStartTime
-echo Build %build% on %platform% start *%time:~0,5%>> testHours.txt
+echo Build %build% on %platform% start *%time:~0,5%>> timelog/%file%.txt
 pause
 echo.
 cls
 goto menu
 
 :recordEndTime
-echo Build %build% on %platform% end *%time:~0,5%>> testHours.txt
+echo Build %build% on %platform% end *%time:~0,5%>> timelog/%file%.txt
 pause
 echo.
 cls
